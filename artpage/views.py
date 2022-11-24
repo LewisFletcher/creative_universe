bbb2e34f6a2f171fabacaff85cead14c1c5cf3fd
Lewis Fletcher <lew.fletcher3@gmail.com>
2022-11-23 17:47:08 -0700

    further commit

--- artpage/views.py
+++ artpage/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.base import View, TemplateView
+from artpage.models import ArtPiece, Collection, ArtType
 # Create your views here.
 
 class ArtView(View):
     def get(self, request):
-        return render(request, 'arthome.html')
+        art_type = ArtType.objects.all()
+        art = ArtPiece.objects.order_by('-uploaded')
+        collections = Collection.objects.all()
+        context = {'arts' : art, 'collections' : collections, 'type' : art_type}
+        return render(request, 'arthome.html', context)
