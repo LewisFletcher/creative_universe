5f32943023def0fedb2e36272241b76540082f8f
Lewis Fletcher <lew.fletcher3@gmail.com>
2024-03-27 20:21:47 -0700

    Removing email for now.

--- creativeuniverse/settings.py
+++ creativeuniverse/settings.py
@@ -213,7 +213,9 @@ AWS_SES_REGION_NAME = 'us-west-2'
 
 AWS_SES_REGION_ENDPOINT = 'email.us-west-2.amazonaws.com'
 
-EMAIL_BACKEND = 'django_ses.SESBackend'
+#EMAIL_BACKEND = 'django_ses.SESBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 LOGGING = {
     'version': 1,
